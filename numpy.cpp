403cd4af749502f20a3d13206747fa020017d1bd
Michael Welter <michael@welter-4d.de>
2016-01-15 14:25:15 +0100

    oops

--- numpy.cpp
+++ numpy.cpp
@@ -1,1 +1,311 @@
-/home/mwelter/tumorcode/src/python_krebsutils/numpy.cpp
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION 
+#include "numpy.hpp"
+#include "numpy/ndarrayobject.h"
+#include "assert.h"
+#include <stdexcept>
+
+namespace py = boost::python;
+
+namespace boost { namespace python { namespace numeric {
+
+namespace mw_py_impl
+{
+
+/* sometimes scalar numpy arrays end up as arguments to c++ function calls. 
+ * For these cases we need automatic conversion functions such as these.
+ * However only conversion to int, float and double is current implemented. 
+ */
+struct int_from_numpyint32
+{
+    static void Register()
+    {
+      boost::python::converter::registry::push_back(
+        &convertible,
+        &construct,
+        boost::python::type_id<int>());
+    }
+
+    static void* convertible(PyObject* obj_ptr)
+    {
+      if (PyArray_IsScalar(obj_ptr, Int32) ||
+          PyArray_IsScalar(obj_ptr, Int64) ||
+          PyArray_IsScalar(obj_ptr, Int16) ||
+          PyArray_IsScalar(obj_ptr, Int8) ||
+          PyArray_IsScalar(obj_ptr, UInt8))
+        return obj_ptr;
+      else
+        return 0;
+    }
+
+    static void construct(
+      PyObject* obj_ptr,
+      boost::python::converter::rvalue_from_python_stage1_data* data)
+    {
+      void* storage = ((boost::python::converter::rvalue_from_python_storage<int>*)data)->storage.bytes;
+      data->convertible = storage;
+      
+      union
+      {
+        int int_data;
+        short short_data;
+        long long longlong_data;
+        char char_data;
+        unsigned char uchar_data;
+        unsigned char buffer[128];
+      } buffer;
+      
+      if (PyArray_IsScalar(obj_ptr, Int8))
+      {
+        PyArray_CastScalarToCtype(obj_ptr, buffer.buffer, PyArray_DescrFromType(NPY_INT8));
+        new (storage) int(buffer.char_data);
+      }
+      else if (PyArray_IsScalar(obj_ptr, UInt8))
+      {
+        PyArray_CastScalarToCtype(obj_ptr, buffer.buffer, PyArray_DescrFromType(NPY_UINT8));
+        new (storage) int(buffer.uchar_data);
+      }
+      else if (PyArray_IsScalar(obj_ptr, UInt16))
+      {
+        PyArray_CastScalarToCtype(obj_ptr, buffer.buffer, PyArray_DescrFromType(NPY_INT16));
+        new (storage) int(buffer.short_data);
+      }
+      else if (PyArray_IsScalar(obj_ptr, Int32))
+      {
+        PyArray_CastScalarToCtype(obj_ptr, buffer.buffer, PyArray_DescrFromType(NPY_INT32));
+        new (storage) int(buffer.int_data);
+      }
+      else if (PyArray_IsScalar(obj_ptr, Int64))
+      {
+        PyArray_CastScalarToCtype(obj_ptr, buffer.buffer, PyArray_DescrFromType(NPY_INT64));
+        new (storage) int(buffer.longlong_data);
+      }
+      else
+        throw std::invalid_argument("numpy int type conversion error");
+    }
+};
+
+template<class T>
+struct from_numpyfloat
+{
+    static void Register()
+    {
+      boost::python::converter::registry::push_back(
+        &convertible,
+        &construct,
+        boost::python::type_id<T>());
+    }
+
+    static void* convertible(PyObject* obj_ptr)
+    {
+      if (!PyArray_IsScalar(obj_ptr, Float32) && !PyArray_IsScalar(obj_ptr, Float64)) return 0;
+      return obj_ptr;
+    }
+
+    static void construct(
+      PyObject* obj_ptr,
+      boost::python::converter::rvalue_from_python_stage1_data* data)
+    {
+      void* storage = ((boost::python::converter::rvalue_from_python_storage<T>*)data)->storage.bytes;
+      data->convertible = storage;
+
+      union
+      {
+        float float_data;
+        double double_data;
+        unsigned char buffer[128];
+      } buffer;
+      
+      if (PyArray_IsScalar(obj_ptr, Float32))
+      {
+        PyArray_CastScalarToCtype(obj_ptr, buffer.buffer, PyArray_DescrFromType(NPY_FLOAT32));
+        new (storage) T(buffer.float_data);
+      }
+      else if (PyArray_IsScalar(obj_ptr, Float64))
+      {
+        PyArray_CastScalarToCtype(obj_ptr, buffer.buffer, PyArray_DescrFromType(NPY_FLOAT64));
+        new (storage) T(buffer.double_data);
+      }
+      else
+        throw std::invalid_argument("numpy int type conversion error");
+    }
+};
+
+PyArrayObject* getPyArrayObjectPtr(py::object &o)
+{
+  return reinterpret_cast<PyArrayObject*>(o.ptr());
+}
+
+const PyArrayObject* getPyArrayObjectPtr(const py::object &o)
+{
+  return reinterpret_cast<const PyArrayObject*>(o.ptr());
+}
+
+} // impl namespace
+
+
+
+using mw_py_impl::getPyArrayObjectPtr;
+
+void importNumpyAndRegisterTypes()
+{
+  import_array1(); // this is from the numpy c-API and import the numpy module into python
+  
+  // these are boost python type conversions
+  mw_py_impl::int_from_numpyint32::Register();
+  mw_py_impl::from_numpyfloat<float>::Register();
+  mw_py_impl::from_numpyfloat<double>::Register();
+  
+  py::numeric::array::set_module_and_type("numpy", "ndarray"); // use numpy
+}
+
+
+array zeros(int rank, const int *dims, int type)
+{
+  npy_intp tmp[NPY_MAXDIMS];
+  for(int i=0; i<rank; ++i) tmp[i] = dims[i];
+  PyObject* p = PyArray_ZEROS(rank,tmp,type,true);
+  return array(handle<>(p));
+}
+
+
+array empty(int rank, const int *dims, int type )
+{
+  npy_intp tmp[NPY_MAXDIMS];
+  for(int i=0; i<rank; ++i) tmp[i] = dims[i];
+  PyObject* p = PyArray_EMPTY(rank, tmp, type, true);
+  return array(handle<>(p));
+}
+
+
+
+int getItemtype(const array &a)
+{ 
+  const PyArrayObject* p = getPyArrayObjectPtr(a);
+  int t = PyArray_TYPE(p);
+  return t;
+}
+
+
+arraytbase::arraytbase() :
+  obj(object())
+{
+
+}
+
+arraytbase::arraytbase(const object& a_) :
+  obj(object()),
+  bytes_(NULL),
+  r(0)
+{
+  memset(m,0,sizeof(int)*MAX_DIM);
+  memset(s,0,sizeof(int)*MAX_DIM);
+
+  if (a_.is_none()) return;
+
+  array a = obj = extract<array>(a_);
+  PyArrayObject* p = getPyArrayObjectPtr(a);
+  
+  bool is_behaved = PyArray_ISBEHAVED(p);
+  if (!is_behaved)
+    throw std::invalid_argument("arrayt: numpy array is not behaved");
+  
+  // directly from the python object
+  //int t = getItemtype(a);
+  r = PyArray_NDIM(p);
+  assert(r < MAX_DIM);
+  itemtype_ = PyArray_TYPE(p);
+  itemsize_ = PyArray_ITEMSIZE(p);
+  bytes_ = (unsigned char*)PyArray_BYTES(p);
+  // copy stuff
+  Py_ssize_t* p_strides = PyArray_STRIDES(p);
+  Py_ssize_t* p_dims = PyArray_DIMS(p);
+  int i=0;
+  for(; i<r; ++i)
+  {
+    m[i] = p_strides[i];
+    s[i] = p_dims[i];
+  }
+  for(; i<MAX_DIM; ++i)
+    s[i] = 1;
+}
+
+
+bool arraytbase::isCContiguous() const
+{
+  const PyArrayObject* p = getPyArrayObjectPtr(obj);
+  return PyArray_IS_C_CONTIGUOUS(p);
+}
+
+
+bool arraytbase::isWriteable() const
+{
+  const PyArrayObject* p = getPyArrayObjectPtr(obj);
+  return PyArray_ISWRITEABLE(p);
+}
+
+
+bool arraytbase::isFContiguous() const
+{
+  const PyArrayObject* p = getPyArrayObjectPtr(obj);
+  return PyArray_IS_F_CONTIGUOUS(p);
+}
+
+template<class T>
+bool isCompatibleType(int id)
+{
+  return false;
+}
+
+#define DEF_TYPE_COMPATIBILITY1(T, npyT1) \
+  template<> bool isCompatibleType<T>(int id)  {  return id == npyT1; }
+#define DEF_TYPE_COMPATIBILITY2(T, npyT1, npyT2) \
+  template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2; }
+#define DEF_TYPE_COMPATIBILITY3(T, npyT1, npyT2, npyT3) \
+  template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2 || id == npyT3; }  
+#define DEF_TYPE_COMPATIBILITY4(T, npyT1, npyT2, npyT3, npyT4) \
+  template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2 || id == npyT3 || id == npyT4; }  
+  
+  
+DEF_TYPE_COMPATIBILITY1(PyObject*, NPY_OBJECT)
+DEF_TYPE_COMPATIBILITY1(bool, NPY_BOOL)
+DEF_TYPE_COMPATIBILITY3(char, NPY_INT8, NPY_BYTE, NPY_BOOL)
+DEF_TYPE_COMPATIBILITY3(unsigned char, NPY_UINT8, NPY_UBYTE, NPY_BOOL)
+DEF_TYPE_COMPATIBILITY2(short, NPY_INT16, NPY_SHORT)
+DEF_TYPE_COMPATIBILITY2(unsigned short, NPY_UINT16, NPY_USHORT)
+DEF_TYPE_COMPATIBILITY2(int, NPY_INT32, NPY_INT)  // NPY_INT is always 32 bit
+DEF_TYPE_COMPATIBILITY2(unsigned int, NPY_UINT32, NPY_UINT)
+// different sizes of type long, see here http://en.cppreference.com/w/cpp/language/types
+#if NPY_BITSOF_LONG == 64 // defined in npy_common.h
+  DEF_TYPE_COMPATIBILITY3(unsigned long, NPY_UINT64, NPY_ULONG, NPY_ULONGLONG)
+  DEF_TYPE_COMPATIBILITY3(long, NPY_INT64, NPY_LONG, NPY_LONGLONG) // NPY_LONG can be NPY_INT or NPY_LONGLONG; NPY_LONGLONG and NPY_INT64 are always 64 bit
+#elif NPY_BITSOF_LONG == 32
+  DEF_TYPE_COMPATIBILITY2(unsigned long, NPY_UINT32, NPY_UINT)
+  DEF_TYPE_COMPATIBILITY2(long, NPY_INT32, NPY_INT)
+#endif
+DEF_TYPE_COMPATIBILITY3(long long, NPY_INT64, NPY_LONG, NPY_LONGLONG)
+DEF_TYPE_COMPATIBILITY3(unsigned long long, NPY_UINT64, NPY_ULONG, NPY_ULONGLONG)
+DEF_TYPE_COMPATIBILITY1(float, NPY_FLOAT32)
+DEF_TYPE_COMPATIBILITY2(double, NPY_FLOAT64, NPY_DOUBLE)
+
+#define DEF_ARRY_TYPE2ID(t,id)\
+template<> int getItemtype<t>() {\
+return id; \
+};
+
+DEF_ARRY_TYPE2ID(PyObject*, NPY_OBJECT);
+DEF_ARRY_TYPE2ID(float,NPY_FLOAT32);
+DEF_ARRY_TYPE2ID(double,NPY_FLOAT64);
+DEF_ARRY_TYPE2ID(int,NPY_INT);
+DEF_ARRY_TYPE2ID(long,NPY_LONG);
+DEF_ARRY_TYPE2ID(long long ,NPY_LONGLONG);
+DEF_ARRY_TYPE2ID(short,NPY_SHORT);
+DEF_ARRY_TYPE2ID(char,NPY_BYTE);
+DEF_ARRY_TYPE2ID(bool,NPY_BOOL); // lets hope that bool is one byte long
+DEF_ARRY_TYPE2ID(unsigned int,NPY_UINT);
+DEF_ARRY_TYPE2ID(unsigned long,NPY_ULONG);
+DEF_ARRY_TYPE2ID(unsigned short,NPY_USHORT);
+DEF_ARRY_TYPE2ID(unsigned char,NPY_UBYTE);
+DEF_ARRY_TYPE2ID(unsigned long long ,NPY_ULONGLONG);
+
+
+} } } // namespace
--- numpy.hpp
+++ numpy.hpp
@@ -1,1 +1,278 @@
-/home/mwelter/tumorcode/src/python_krebsutils/numpy.hpp
+#ifndef __CXX_NUMPY_H__
+#define __CXX_NUMPY_H__
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
+#include <boost/python.hpp>
+#include <boost/python/object.hpp>
+//#include <boost/mpl/int.hpp>
+#include "assert.h"
+
+namespace boost { namespace python { namespace numeric {
+
+namespace mw_py_impl
+{
+  // see here http://stackoverflow.com/questions/16454987/c-generate-a-compile-error-if-user-tries-to-instantiate-a-class-template-wiho
+  template<class T>
+  class incomplete;
+}
+
+  
+void importNumpyAndRegisterTypes();
+  
+enum { MAX_DIM = 32 };
+  
+// see here http://stackoverflow.com/questions/16454987/c-generate-a-compile-error-if-user-tries-to-instantiate-a-class-template-wiho
+template<class T>
+int getItemtype()
+{
+  enum { S = sizeof(mw_py_impl::incomplete<T>) }; // will generate an error
+  return -1;
+}
+
+#define NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(T)\
+  template<> int getItemtype<T>();
+  
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(PyObject*)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(float)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(double)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(int)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(long)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(long long)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(short)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(char)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(bool)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(unsigned int)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(unsigned long)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(unsigned short)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(unsigned char)
+NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE(unsigned long long)
+#undef NUMPY_HPP_FWD_DECLARE_GET_ITEMTYPE
+
+
+array zeros(int rank, const int *dims, int type);
+array empty(int rank, const int *dims, int type);
+
+int getItemtype(const array &arr);
+
+template<class T>
+bool isCompatibleType(int id);
+
+class arraytbase
+{ 
+protected:
+  array obj; // the object
+  int r, itemsize_, itemtype_; // rank
+  int m[MAX_DIM],s[MAX_DIM]; // strides and size
+  unsigned char* bytes_; // data
+  bool is_ok_;
+
+public:
+  arraytbase();
+  arraytbase(object const &a);
+  
+  const int* shape() const { return s; }
+  int itemtype() const { return itemtype_; }
+  int itemsize() const { return itemsize_; }
+  int rank() const { return r; }
+  const int* strides() const { return m; } // in number of bytes, *not* number of items!
+  bool isWriteable() const;
+  bool isCContiguous() const;
+  bool isFContiguous() const;
+  const array& getObject() const { return obj; }
+  
+  inline int offset(int x) const
+  {
+    assert((unsigned int)x<s[0]);
+    return x*m[0];
+  }
+  
+  inline int offset(int x, int y) const
+  {
+    assert((unsigned int)y<s[1]);
+    return offset(x) + y*m[1];
+  }
+  
+  inline int offset(int x, int y, int z) const
+  {
+    assert((unsigned int)z<s[2]);
+    return offset(x,y) + z*m[2];
+  }
+  
+  inline int offset(int x,int y, int z, int w) const
+  {
+    assert((unsigned int)w<s[3]);
+    return offset(x,y,z) + w*m[3];
+  }
+
+  inline int offset(const int *c) const
+  {
+    int q = 0;
+    for(int i=0; i<r; ++i)
+    {
+      assert((unsigned int)(c[i])<s[i]);
+      q += c[i]*m[i];
+    }
+    return q;
+  }
+
+  unsigned char* bytes()
+  {
+    return bytes_;
+  }
+};
+
+
+template<class T>
+class arrayt : public arraytbase
+{
+public:
+  arrayt() : arraytbase() {}
+  arrayt(arraytbase const &a) : arraytbase(a.getObject()) 
+  {
+    if (sizeof(T) != arraytbase::itemsize())
+      throw std::invalid_argument("arrayt: array itemsize does not match template argument");
+  }
+  arrayt(object const &a) : arraytbase(a)
+  {
+    if (sizeof(T) != arraytbase::itemsize())
+      throw std::invalid_argument("arrayt: array itemsize does not match template argument");
+  }
+
+  void init(object const &a_)
+  {
+    this->~arrayt<T>();
+    new (this) arrayt<T>(a_);
+  }
+
+  T& operator()(int x)
+  {
+    return *((T*)(arraytbase::bytes_+offset(x)));
+  }
+  
+  T& operator()(int x, int y)
+  {
+    return *((T*)(arraytbase::bytes_+offset(x,y)));
+  }
+  
+  T& operator()(int x, int y, int z)
+  {
+    return *((T*)(arraytbase::bytes_+offset(x,y,z)));
+  }
+  
+  T& operator()(int x, int y, int z, int w)
+  {
+    return *((T*)(arraytbase::bytes_+offset(x,y,z,w)));
+  }
+  
+  T& operator()(int *c)
+  {
+    return *((T*)(arraytbase::bytes_+offset(c)));
+  }
+  
+  T& operator[](int i)
+  {
+    return *((T*)(arraytbase::bytes_+offset(i, 0, 0, 0)));
+  }
+
+  T* data() { return reinterpret_cast<T*>(bytes()); }
+  
+  
+  T operator()(int x) const
+  {
+    return *((T*)(arraytbase::bytes_+offset(x)));
+  }
+  
+  T operator()(int x, int y) const
+  {
+    return *((T*)(arraytbase::bytes_+offset(x,y)));
+  }
+  
+  T operator()(int x, int y, int z) const
+  {
+    return *((T*)(arraytbase::bytes_+offset(x,y,z)));
+  }
+  
+  T operator()(int x, int y, int z, int w) const
+  {
+    return *((T*)(arraytbase::bytes_+offset(x,y,z,w)));
+  }
+  
+  T operator()(int *c) const
+  {
+    return *((T*)(arraytbase::bytes_+offset(c)));
+  }
+  
+  T operator[](int i) const
+  {
+    return *((T*)(arraytbase::bytes_+offset(i, 0, 0, 0)));
+  }
+  
+  const T* data() const { return reinterpret_cast<T*>(bytes()); }
+};
+
+
+namespace mw_py_impl
+{
+
+template<class T>
+inline void gridded_data_ccons(T* dst, const T* src, const int* dims, const int *dst_strides, const int *src_strides, boost::mpl::int_<0>)
+{
+  for (int i=0; i<dims[0]; ++i)
+  {
+    new (dst) T(*src);
+    dst += dst_strides[0];
+    src += src_strides[0];
+  }
+}
+
+template<class T, int dim>
+inline void gridded_data_ccons(T* dst, const T* src, const int* dims, const int *dst_strides, const int *src_strides, boost::mpl::int_<dim>)
+{
+  for (int i=0; i<dims[dim]; ++i)
+  {
+    gridded_data_ccons(dst, src, dims, dst_strides, src_strides, boost::mpl::int_<dim-1>());
+    dst += dst_strides[dim];
+    src += src_strides[dim];
+  }
+}
+
+}
+
+
+/*
+ * WARNING: strides given in number of items, *not in bytes*
+ */
+template<class T, int rank>
+static array copy(const int* dims, const T* src, const int *strides)
+{
+  int item_type = getItemtype<T>();
+  arrayt<T> arr(empty(rank, dims, item_type));
+
+  int arr_strides[MAX_DIM];
+  for (int i=0; i<rank; ++i) arr_strides[i] = arr.strides()[i]/arr.itemsize();
+  
+  T* dst = arr.data();
+  mw_py_impl::gridded_data_ccons<T>(dst, src, dims, arr_strides, strides, boost::mpl::int_<rank-1>());
+  
+  return arr.getObject();
+}
+
+template<class T, int rank>
+static void copy(T* dst, const int *dims, const int* strides, const array &pyarr)
+{
+  arrayt<T> arr(pyarr);
+
+  int arr_strides[MAX_DIM];
+  for (int i=0; i<rank; ++i) {
+    assert(arr.shape()[i] == dims[i]);
+    arr_strides[i] = arr.strides()[i]/arr.itemsize();
+  }
+  
+  const T* src = arr.data();
+  mw_py_impl::gridded_data_ccons<T>(dst, src, arr.shape(), strides, arr_strides, boost::mpl::int_<rank-1>());
+}
+
+
+} } }
+
+#endif
