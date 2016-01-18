fa3220c76ae2ec5d26d9f912f938b110d6cee827
Michael Welter <michael@welter-4d.de>
2016-01-18 19:23:18 +0100

    license BS

--- demo.cpp
+++ demo.cpp
@@ -1,9 +1,24 @@
+/*
+ C *opyright (c) 2016 Michael Welter
+ 
+ This file is part of numpycpp.
+ 
+ numpycpp is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ numpycpp is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with numpycpp.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <iostream> 
 #include <sstream>
 #include <boost/python/object.hpp>
-//#include <boost/python/tuple.hpp>
-//#include <boost/python/str.hpp>
-//#include <boost/python/exec.hpp>
 #include "numpy.hpp"
 
 namespace py = boost::python;
