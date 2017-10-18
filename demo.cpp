60177188f3fc2f7e8640512f83ae90feca66c98b
Thierry Fredrich <thierry@lusi.uni-sb.de>
2017-10-18 14:53:33 +0200

    demo works without numpycpp, imortant to see the new API of boost::numpy

--- demo.cpp
+++ demo.cpp
@@ -18,67 +18,13 @@
  */
 #include <iostream> 
 #include <sstream>
-#include <boost/python.hpp>
 #include <boost/python/object.hpp>
-#if BOOST_VERSION>106300
-  #include <boost/python/numpy.hpp>
-  #include <boost/python/numpy/ndarray.hpp>
-  #include "/usr/lib/python2.7/site-packages/numpy/core/include/numpy/ndarrayobject.h"
-  namespace np = boost::python::numpy;
-#else
-  #include "numpy.hpp"
-  namespace nm = boost::python::numeric;
-#endif
+#include "numpy.hpp"
 
 namespace py = boost::python;
-
-
+namespace np = boost::python::numpy;
+namespace nm = boost::python::numeric;
 using namespace std;
-
-// #define CAST_TO_PPYARRAYOBJECT(p) ((PyArrayObject*)(p))
-// template<class T>
-// bool isCompatibleType(int id)
-// {
-//   return false;
-// }
-// 
-// /** @cond */
-// #define DEF_TYPE_COMPATIBILITY1(T, npyT1) \
-//   template<> bool isCompatibleType<T>(int id)  {  return id == npyT1; }
-// #define DEF_TYPE_COMPATIBILITY2(T, npyT1, npyT2) \
-//   template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2; }
-// #define DEF_TYPE_COMPATIBILITY3(T, npyT1, npyT2, npyT3) \
-//   template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2 || id == npyT3; }  
-// #define DEF_TYPE_COMPATIBILITY4(T, npyT1, npyT2, npyT3, npyT4) \
-//   template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2 || id == npyT3 || id == npyT4; }  
-//   
-//   
-// DEF_TYPE_COMPATIBILITY1(PyObject*, NPY_OBJECT)
-// DEF_TYPE_COMPATIBILITY1(bool, NPY_BOOL)
-// DEF_TYPE_COMPATIBILITY3(char, NPY_INT8, NPY_BYTE, NPY_BOOL)
-// DEF_TYPE_COMPATIBILITY3(unsigned char, NPY_UINT8, NPY_UBYTE, NPY_BOOL)
-// DEF_TYPE_COMPATIBILITY2(short, NPY_INT16, NPY_SHORT)
-// DEF_TYPE_COMPATIBILITY2(unsigned short, NPY_UINT16, NPY_USHORT)
-// DEF_TYPE_COMPATIBILITY2(int, NPY_INT32, NPY_INT)  // NPY_INT is always 32 bit
-// DEF_TYPE_COMPATIBILITY2(unsigned int, NPY_UINT32, NPY_UINT)
-// // different sizes of type long, see here http://en.cppreference.com/w/cpp/language/types
-// #if NPY_BITSOF_LONG == 64 // defined in npy_common.h
-//   DEF_TYPE_COMPATIBILITY3(unsigned long, NPY_UINT64, NPY_ULONG, NPY_ULONGLONG)
-//   DEF_TYPE_COMPATIBILITY3(long, NPY_INT64, NPY_LONG, NPY_LONGLONG) // NPY_LONG can be NPY_INT or NPY_LONGLONG; NPY_LONGLONG and NPY_INT64 are always 64 bit
-// #elif NPY_BITSOF_LONG == 32
-//   DEF_TYPE_COMPATIBILITY2(unsigned long, NPY_UINT32, NPY_UINT)
-//   DEF_TYPE_COMPATIBILITY2(long, NPY_INT32, NPY_INT)
-// #endif
-// DEF_TYPE_COMPATIBILITY3(long long, NPY_INT64, NPY_LONG, NPY_LONGLONG)
-// DEF_TYPE_COMPATIBILITY3(unsigned long long, NPY_UINT64, NPY_ULONG, NPY_ULONGLONG)
-// DEF_TYPE_COMPATIBILITY1(float, NPY_FLOAT32)
-// DEF_TYPE_COMPATIBILITY2(double, NPY_FLOAT64, NPY_DOUBLE)
-
-/** @brief Equal to NPY_MAX_DIMS. Provided so that the numpy headers don't have to be included.
-*/
-enum {
-  MAX_DIM = 32
-};
 
 template<class T>
 void print(const T &x, std::ostream &os)
@@ -101,49 +47,9 @@ void print<PyObject*>(PyObject* const &x, std::ostream &os)
   std::string s = py::extract<std::string>(py::str(p));
   cout << s;
 }
-#if BOOST_VERSION>106300
-#else
-int getItemtype(const py::object &a)
-{ 
-  if (PyArray_Check(a.ptr()))
-  {
-    const PyArrayObject* p = CAST_TO_PPYARRAYOBJECT(a.ptr());
-    int t = PyArray_TYPE(p);
-    return t;
-  }
-  else
-    return -1;
-}
 
-#define DEF_ARRY_TYPE2ID(t,id)\
-template<> int getItemtype<t>() {\
-return id; \
-};
 
-#endif 
 
-#if BOOST_VERSION>106300
-void printElements(const np::ndarray &a, int dim, int *indices)
-{
-  if (dim >= a.get_nd())
-  {
-    cout << "(";
-    int i = 0;
-    while (i<a.get_nd()-1)  cout << indices[i++] << ',';
-    cout << indices[i] << ") = ";
-    print(py::extract<int>(a[indices]), cout); 
-    cout << endl;
-  }
-  else
-  {
-    const int n = a.get_shape()[dim];
-    for (indices[dim]=0; indices[dim]<n; ++indices[dim])
-    {
-      printElements(a, dim+1, indices);
-    }
-  }
-}
-#else
 template<class T>
 void printElements(const np::arrayt<T> &a, int dim, int *indices)
 {
@@ -165,31 +71,7 @@ void printElements(const np::arrayt<T> &a, int dim, int *indices)
     }
   }
 }
-#endif
 
-#if BOOST_VERSION>106300
-template<class T>
-void handleType(np::ndarray arr, bool printContents)
-{
-  if (np::equivalent(np::dtype::get_builtin<T>(), arr.get_dtype()))
-  {
-    cout << "data of type: <" << typeid(T).name() << "> will be handeled" << endl;
-  }
-//   if (isCompatibleType<T>(arr.get_dtype()))
-//   {
-    if (printContents)
-    {
-      cout << "Printing array of type " << typeid(T).name() << endl;
-      int indices[MAX_DIM];
-      printElements(np::ndarray(arr), 0, indices);
-    }
-    else
-    {
-      cout << "mapped c++ type = " << typeid(T).name() << endl;
-    }
-//   }    
-}
-#else
 template<class T>
 void handleType(np::arraytbase arr, bool printContents)
 {
@@ -211,28 +93,7 @@ void handleType(np::arraytbase arr, bool printContents)
     }
   }    
 }
-#endif
 
-#if BOOST_VERSION>106300
-void printArrayInfo(const np::ndarray &arr)
-{
-  int i;
-  cout << "get_shape()          = " << arr.get_shape() << endl;
-  //cout << "get_dtype()          = " << arr.get_dtype() << endl;
-  //cout << "itemsize()      = " << arr.itemsize() << endl;
-  //cout << "isWriteable()   = " << arr.isWriteable() << endl;
-  //cout << "isCContiguous() = " << arr.isCContiguous() << endl;
-  //cout << "isFContiguous() = " << arr.isFContiguous() << endl;
-  cout << "shape()         = ";
-  for (i=0; i<arr.get_nd()-1; ++i)
-    cout << arr.shape(i) << ',';
-  cout << arr.shape(i) << endl;
-  cout << "strides()       = ";
-  for (i=0; i<arr.get_nd()-1; ++i)
-    cout << arr.strides(i) << ',';
-  cout << arr.strides(i) << endl;
-}
-#else
 void printArrayInfo(const np::arraytbase &arr)
 {
   int i;
@@ -251,33 +112,8 @@ void printArrayInfo(const np::arraytbase &arr)
     cout << arr.strides()[i] << ',';
   cout << arr.strides()[i] << endl;
 }
-#endif
 
-#if BOOST_VERSION>106300
-void printArray(np::ndarray pyarr, bool printContents)
-{
-  np::ndarray arr(pyarr);
-  printArrayInfo(arr);
-  
-//   handleType<float>(arr, printContents);
-//   handleType<PyObject*>(arr, printContents);
-  
-//   handleType<double>(arr, printContents);
-//   
-//   handleType<bool>(arr, printContents);
-//   handleType<int>(arr, printContents);
-//   handleType<short>(arr, printContents);
-//   handleType<char>(arr, printContents);
-//   handleType<long>(arr, printContents);
-//   handleType<long long>(arr, printContents);
-//   
-//   handleType<unsigned int>(arr, printContents);
-//   handleType<unsigned short>(arr, printContents);
-//   handleType<unsigned char>(arr, printContents);
-//   handleType<unsigned long>(arr, printContents);
-//   handleType<unsigned long long>(arr, printContents);
-}
-#else
+
 void printArray(nm::array pyarr, bool printContents)
 {
   np::arraytbase arr(pyarr);
@@ -300,30 +136,20 @@ void printArray(nm::array pyarr, bool printContents)
   handleType<unsigned long>(arr, printContents);
   handleType<unsigned long long>(arr, printContents);
 }
-#endif
 
-#if BOOST_VERSION>106300
-void printConvertedArray(np::ndarray arr)
-{
-  printArrayInfo(arr);
-}
-#else
+
 template<class T>
 void printConvertedArray(np::arrayt<T> arr)
 {
   printArrayInfo(arr);
   handleType<T>(arr, true);
 }
-#endif
 
 template<class T>
 std::string scalar_to_str(const T &x)
 {
   std::ostringstream os;
   os << x;
-//   os << py::extract<T>(x);
-//   os << py::extract<T>(x);
-//   return std::string(py::extract<T>(x));
   return os.str();
 }
 
@@ -335,15 +161,7 @@ std::string scalar_to_str(const char &x)
   return os.str();
 }
 
-#if BOOST_VERSION>106300
-np::ndarray ReturnedFromCPP()
-{
-  np::ndarray ret = np::zeros(py::make_tuple(5), np::dtype::get_builtin<float>());
-  for( int i=0; i<5; ++i)
-    ret[i] = i;
-  return ret;
-}
-#else
+
 np::arraytbase ReturnedFromCPP()
 {
   np::ssize_t dims = 5;
@@ -352,11 +170,8 @@ np::arraytbase ReturnedFromCPP()
     ret[i] = i;
   return ret;
 }
-#endif
 
-#if BOOST_VERSION>106300
 
-#else
 py::object SumArrayT(np::arrayt<float> arr1, np::arrayt<float>  arr2)
 {
   np::arrayt<float> ret(np::empty(arr1.rank(), arr1.dims(), arr1.itemtype()));
@@ -368,34 +183,11 @@ py::object SumArrayT(np::arrayt<float> arr1, np::arrayt<float>  arr2)
   }
   return ret.getObject();
 }
-#endif
 
-#if BOOST_VERSION>106300
-py::object SumNumericArray(np::ndarray arr1, np::ndarray arr2)
-{
-  //nm::array ret = py::extract<nm::array>(np::empty(1, &len, np::getItemtype(arr1)));
-  //const Py_ssize_t len = py::extract<int>(py::getattr(arr1, "shape")[0]);
-  //const Py_ssize_t len = arr1.get_nd();
-  py::tuple shape = py::make_tuple(1,10);
-  //np::dtype dtype = np::dtype::get_builtin<float>();
-  np::ndarray ret = np::empty(shape,arr1.get_dtype());
-  
-  for (int i=0; i<arr1.get_nd(); ++i)
-  {
-    float x1 = py::extract<float>(arr1[i]);
-    float x2 = py::extract<float>(arr2[i]);
-    ret[i] = py::object(x1 + x2);
-  }
-  return ret;
-}
-#else
 py::object SumNumericArray(nm::array arr1, nm::array arr2)
 {
   const np::ssize_t len = py::extract<int>(py::getattr(arr1, "shape")[0]);
   nm::array ret = py::extract<nm::array>(np::empty(1, &len, np::getItemtype(arr1)));
-  //py::tuple shape = py::make_tuple(1,len);
-  //np::dtype dtype = np::dtype::get_builtin<float>();
-  //np::ndarray ret = np::empty(shape,dtype);
   for (int i=0; i<len; ++i)
   {
     float x1 = py::extract<float>(arr1[i]);
@@ -404,30 +196,19 @@ py::object SumNumericArray(nm::array arr1, nm::array arr2)
   }
   return ret;
 }
-#endif
+
 
 
 BOOST_PYTHON_MODULE(libdemo)
 {
-  Py_Initialize();
-#if BOOST_VERSION>106300
-  cout<<"new boost numpy support detected"<<endl;
-  np::initialize();
-  py::def("printConvertedArray_int", printConvertedArray);
-#else
-  cout<<"NO boost numpy support detected"<<endl;
   np::importNumpyAndRegisterTypes();
-  py::def("printConvertedArray_int", printConvertedArray<int>);
-  py::def("SumArrayT", SumArrayT);
-#endif
-  
   py::def("printArray", printArray);
-  
-  py::def("int_to_str", scalar_to_str<int>);
+  py::def("printConvertedArray_int", printConvertedArray<int>);
   py::def("char_to_str", scalar_to_str<char>);
+  py::def("int_to_str", scalar_to_str<int>);
   py::def("float_to_str", scalar_to_str<float>);
   py::def("double_to_str", scalar_to_str<double>);
-  
+  py::def("SumArrayT", SumArrayT);
   py::def("SumNumericArray", SumNumericArray);
   py::def("ReturnedFromCPP", ReturnedFromCPP);
 }
--- demo_boost_numpy.cpp
+++ demo_boost_numpy.cpp
@@ -20,59 +20,18 @@
 #include <sstream>
 #include <boost/python.hpp>
 #include <boost/python/object.hpp>
-#if BOOST_VERSION>106300
-  #include <boost/python/numpy.hpp>
-  #include <boost/python/numpy/ndarray.hpp>
-  #include "/usr/lib/python2.7/site-packages/numpy/core/include/numpy/ndarrayobject.h"
-  namespace np = boost::python::numpy;
-#else
-  #include "numpy.hpp"
-  namespace nm = boost::python::numeric;
-#endif
 
+#include <boost/python/numpy.hpp>
+#include <boost/python/numpy/ndarray.hpp>
+
+
+namespace np = boost::python::numpy;
 namespace py = boost::python;
 
 
 using namespace std;
 
-// #define CAST_TO_PPYARRAYOBJECT(p) ((PyArrayObject*)(p))
-// template<class T>
-// bool isCompatibleType(int id)
-// {
-//   return false;
-// }
-// 
-// /** @cond */
-// #define DEF_TYPE_COMPATIBILITY1(T, npyT1) \
-//   template<> bool isCompatibleType<T>(int id)  {  return id == npyT1; }
-// #define DEF_TYPE_COMPATIBILITY2(T, npyT1, npyT2) \
-//   template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2; }
-// #define DEF_TYPE_COMPATIBILITY3(T, npyT1, npyT2, npyT3) \
-//   template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2 || id == npyT3; }  
-// #define DEF_TYPE_COMPATIBILITY4(T, npyT1, npyT2, npyT3, npyT4) \
-//   template<> bool isCompatibleType<T>(int id)  {  return id == npyT1 || id == npyT2 || id == npyT3 || id == npyT4; }  
-//   
-//   
-// DEF_TYPE_COMPATIBILITY1(PyObject*, NPY_OBJECT)
-// DEF_TYPE_COMPATIBILITY1(bool, NPY_BOOL)
-// DEF_TYPE_COMPATIBILITY3(char, NPY_INT8, NPY_BYTE, NPY_BOOL)
-// DEF_TYPE_COMPATIBILITY3(unsigned char, NPY_UINT8, NPY_UBYTE, NPY_BOOL)
-// DEF_TYPE_COMPATIBILITY2(short, NPY_INT16, NPY_SHORT)
-// DEF_TYPE_COMPATIBILITY2(unsigned short, NPY_UINT16, NPY_USHORT)
-// DEF_TYPE_COMPATIBILITY2(int, NPY_INT32, NPY_INT)  // NPY_INT is always 32 bit
-// DEF_TYPE_COMPATIBILITY2(unsigned int, NPY_UINT32, NPY_UINT)
-// // different sizes of type long, see here http://en.cppreference.com/w/cpp/language/types
-// #if NPY_BITSOF_LONG == 64 // defined in npy_common.h
-//   DEF_TYPE_COMPATIBILITY3(unsigned long, NPY_UINT64, NPY_ULONG, NPY_ULONGLONG)
-//   DEF_TYPE_COMPATIBILITY3(long, NPY_INT64, NPY_LONG, NPY_LONGLONG) // NPY_LONG can be NPY_INT or NPY_LONGLONG; NPY_LONGLONG and NPY_INT64 are always 64 bit
-// #elif NPY_BITSOF_LONG == 32
-//   DEF_TYPE_COMPATIBILITY2(unsigned long, NPY_UINT32, NPY_UINT)
-//   DEF_TYPE_COMPATIBILITY2(long, NPY_INT32, NPY_INT)
-// #endif
-// DEF_TYPE_COMPATIBILITY3(long long, NPY_INT64, NPY_LONG, NPY_LONGLONG)
-// DEF_TYPE_COMPATIBILITY3(unsigned long long, NPY_UINT64, NPY_ULONG, NPY_ULONGLONG)
-// DEF_TYPE_COMPATIBILITY1(float, NPY_FLOAT32)
-// DEF_TYPE_COMPATIBILITY2(double, NPY_FLOAT64, NPY_DOUBLE)
+
 
 /** @brief Equal to NPY_MAX_DIMS. Provided so that the numpy headers don't have to be included.
 */
@@ -83,93 +42,65 @@ enum {
 template<class T>
 void print(const T &x, std::ostream &os)
 {
-  os << x;
+//   cout<< "in templated print" << endl;
+//   cout.flush();
+  std::string s = py::extract<std::string>(py::str(x));
+  os << s;
+//   os << x;
 }
 
 template<>
 void print<char>(const char &x, std::ostream &os)
 {
+//   cout<< "in char print" << endl;
+//   cout.flush();
   os << (int)x;
 }
 
 template<>
 void print<PyObject*>(PyObject* const &x, std::ostream &os)
 {
+//   cout<< "in py print" << endl;
+//   cout.flush();
   // increase reference count, so the object is not destructed when p goes out of scope. 
   // See http://www.boost.org/doc/libs/1_53_0/libs/python/doc/tutorial/doc/html/python/object.html#python.creating_python_object
   py::object p(py::borrowed(x));
   std::string s = py::extract<std::string>(py::str(p));
   cout << s;
 }
-#if BOOST_VERSION>106300
-#else
-int getItemtype(const py::object &a)
-{ 
-  if (PyArray_Check(a.ptr()))
-  {
-    const PyArrayObject* p = CAST_TO_PPYARRAYOBJECT(a.ptr());
-    int t = PyArray_TYPE(p);
-    return t;
-  }
-  else
-    return -1;
-}
 
-#define DEF_ARRY_TYPE2ID(t,id)\
-template<> int getItemtype<t>() {\
-return id; \
-};
-
-#endif 
-
-#if BOOST_VERSION>106300
+template<class T>
 void printElements(const np::ndarray &a, int dim, int *indices)
 {
-  if (dim >= a.get_nd())
-  {
-    cout << "(";
-    int i = 0;
-    while (i<a.get_nd()-1)  cout << indices[i++] << ',';
-    cout << indices[i] << ") = ";
-    print(py::extract<int>(a[indices]), cout); 
-    cout << endl;
-  }
-  else
-  {
-    const int n = a.get_shape()[dim];
-    for (indices[dim]=0; indices[dim]<n; ++indices[dim])
-    {
-      printElements(a, dim+1, indices);
-    }
-  }
+//   cout << "in printElements " << endl;
+//   cout << "dim: " << dim << ", a.get_nd() " << a.get_nd() << endl;
+  
+  //T.F. unfortunatelly I am not able to access singel elements???
+  std::cout << "Original array:\n" << py::extract<char const *>(py::str(a)) << std::endl;
+//   if (dim > a.get_nd())
+//   {
+//     cout << "(";
+//     int i = 0;
+//     while (i<a.get_nd()-1)  cout << indices[i++] << ',';
+//     cout << indices[i] << ") = ";
+//     //note: this breaks!!!
+//     int read = py::extract<T>(a[0]);
+//     cout << endl;
+//   }
+//   else
+//   {
+//     const int n = a.shape(dim);
+//     for (indices[dim]=0; indices[dim]<n; ++indices[dim])
+//     {
+//       printElements<T>(a, dim+1, indices);
+//     }
+//   }
 }
-#else
-template<class T>
-void printElements(const np::arrayt<T> &a, int dim, int *indices)
-{
-  if (dim >= a.rank())
-  {
-    cout << "(";
-    int i = 0;
-    while (i<a.rank()-1)  cout << indices[i++] << ',';
-    cout << indices[i] << ") = ";
-    print(a(indices), cout); 
-    cout << endl;
-  }
-  else
-  {
-    const int n = a.shape()[dim];
-    for (indices[dim]=0; indices[dim]<n; ++indices[dim])
-    {
-      printElements(a, dim+1, indices);
-    }
-  }
-}
-#endif
 
-#if BOOST_VERSION>106300
+
+
 template<class T>
-void handleType(np::ndarray arr, bool printContents)
+void handleType(np::ndarray &arr, bool printContents)
 {
   if (np::equivalent(np::dtype::get_builtin<T>(), arr.get_dtype()))
   {
@@ -181,7 +112,7 @@ void handleType(np::ndarray arr, bool printContents)
     {
       cout << "Printing array of type " << typeid(T).name() << endl;
       int indices[MAX_DIM];
-      printElements(np::ndarray(arr), 0, indices);
+      printElements<T>(arr, 0, indices);
     }
     else
     {
@@ -189,34 +120,12 @@ void handleType(np::ndarray arr, bool printContents)
     }
 //   }    
 }
-#else
-template<class T>
-void handleType(np::arraytbase arr, bool printContents)
-{
-  if (np::getItemtype<T>() == arr.itemtype())
-  {
-    cout << "getItemtype<" << typeid(T).name() << "> matches type of array" << endl;
-  }
-  if (np::isCompatibleType<T>(arr.itemtype()))
-  {
-    if (printContents)
-    {
-      cout << "Printing array of type " << typeid(T).name() << endl;
-      int indices[np::MAX_DIM];
-      printElements(np::arrayt<T>(arr), 0, indices);
-    }
-    else
-    {
-      cout << "mapped c++ type = " << typeid(T).name() << endl;
-    }
-  }    
-}
-#endif
 
-#if BOOST_VERSION>106300
+
 void printArrayInfo(const np::ndarray &arr)
 {
   int i;
+  cout << "we are in printArrayInfo" << endl;
   cout << "get_shape()          = " << arr.get_shape() << endl;
   //cout << "get_dtype()          = " << arr.get_dtype() << endl;
   //cout << "itemsize()      = " << arr.itemsize() << endl;
@@ -232,89 +141,43 @@ void printArrayInfo(const np::ndarray &arr)
     cout << arr.strides(i) << ',';
   cout << arr.strides(i) << endl;
 }
-#else
-void printArrayInfo(const np::arraytbase &arr)
-{
-  int i;
-  cout << "rank()          = " << arr.rank() << endl;
-  cout << "itemtype()      = " << arr.itemtype() << endl;
-  cout << "itemsize()      = " << arr.itemsize() << endl;
-  cout << "isWriteable()   = " << arr.isWriteable() << endl;
-  cout << "isCContiguous() = " << arr.isCContiguous() << endl;
-  cout << "isFContiguous() = " << arr.isFContiguous() << endl;
-  cout << "shape()         = ";
-  for (i=0; i<arr.rank()-1; ++i)
-    cout << arr.shape()[i] << ',';
-  cout << arr.shape()[i] << endl;
-  cout << "strides()       = ";
-  for (i=0; i<arr.rank()-1; ++i)
-    cout << arr.strides()[i] << ',';
-  cout << arr.strides()[i] << endl;
-}
-#endif
 
-#if BOOST_VERSION>106300
-void printArray(np::ndarray pyarr, bool printContents)
-{
-  np::ndarray arr(pyarr);
-  printArrayInfo(arr);
-  
-//   handleType<float>(arr, printContents);
-//   handleType<PyObject*>(arr, printContents);
-  
-//   handleType<double>(arr, printContents);
-//   
-//   handleType<bool>(arr, printContents);
-//   handleType<int>(arr, printContents);
-//   handleType<short>(arr, printContents);
-//   handleType<char>(arr, printContents);
-//   handleType<long>(arr, printContents);
-//   handleType<long long>(arr, printContents);
-//   
-//   handleType<unsigned int>(arr, printContents);
-//   handleType<unsigned short>(arr, printContents);
-//   handleType<unsigned char>(arr, printContents);
-//   handleType<unsigned long>(arr, printContents);
-//   handleType<unsigned long long>(arr, printContents);
-}
-#else
-void printArray(nm::array pyarr, bool printContents)
-{
-  np::arraytbase arr(pyarr);
-  printArrayInfo(arr);
-  
-  handleType<PyObject*>(arr, printContents);
-  handleType<float>(arr, printContents);
-  handleType<double>(arr, printContents);
-  
-  handleType<bool>(arr, printContents);
-  handleType<int>(arr, printContents);
-  handleType<short>(arr, printContents);
-  handleType<char>(arr, printContents);
-  handleType<long>(arr, printContents);
-  handleType<long long>(arr, printContents);
-  
-  handleType<unsigned int>(arr, printContents);
-  handleType<unsigned short>(arr, printContents);
-  handleType<unsigned char>(arr, printContents);
-  handleType<unsigned long>(arr, printContents);
-  handleType<unsigned long long>(arr, printContents);
-}
-#endif
 
-#if BOOST_VERSION>106300
-void printConvertedArray(np::ndarray arr)
+
+void printArray(np::ndarray &pyarr, bool printContents)
 {
-  printArrayInfo(arr);
+  
+  printArrayInfo(pyarr);
+  
+  handleType<float>(pyarr, printContents);
+  handleType<PyObject*>(pyarr, printContents);
+  
+  handleType<double>(pyarr, printContents);
+  
+  handleType<bool>(pyarr, printContents);
+  handleType<int>(pyarr, printContents);
+  handleType<short>(pyarr, printContents);
+  handleType<char>(pyarr, printContents);
+  handleType<long>(pyarr, printContents);
+  handleType<long long>(pyarr, printContents);
+  
+  handleType<unsigned int>(pyarr, printContents);
+  handleType<unsigned short>(pyarr, printContents);
+  handleType<unsigned char>(pyarr, printContents);
+  handleType<unsigned long>(pyarr, printContents);
+  handleType<unsigned long long>(pyarr, printContents);
 }
-#else
+
+
+
 template<class T>
-void printConvertedArray(np::arrayt<T> arr)
+void printConvertedArray(np::ndarray &arr)
 {
   printArrayInfo(arr);
+  cout << " going int handleType " <<endl;
   handleType<T>(arr, true);
 }
-#endif
+
 
 template<class T>
 std::string scalar_to_str(const T &x)
@@ -335,7 +198,6 @@ std::string scalar_to_str(const char &x)
   return os.str();
 }
 
-#if BOOST_VERSION>106300
 np::ndarray ReturnedFromCPP()
 {
   np::ndarray ret = np::zeros(py::make_tuple(5), np::dtype::get_builtin<float>());
@@ -343,44 +205,36 @@ np::ndarray ReturnedFromCPP()
     ret[i] = i;
   return ret;
 }
-#else
-np::arraytbase ReturnedFromCPP()
-{
-  np::ssize_t dims = 5;
-  np::arrayt<float> ret(np::empty(1, &dims, np::getItemtype<float>()));
-  for (int i=0; i<dims; ++i)
-    ret[i] = i;
-  return ret;
-}
-#endif
 
-#if BOOST_VERSION>106300
+/* this function is no longer needed */
+// py::object SumArrayT(np::ndarray arr1, np::ndarray arr2)
+// {
+//   np::arrayt<float> ret(np::empty(arr1.rank(), arr1.dims(), arr1.itemtype()));
+//   
+//   const np::ssize_t *dims = arr1.dims();
+//   for (int i=0; i<dims[0]; ++i)
+//   {
+//     ret(i) = arr1(i) + arr2(i);
+//   }
+//   return ret.getObject();
+// }
 
-#else
-py::object SumArrayT(np::arrayt<float> arr1, np::arrayt<float>  arr2)
-{
-  np::arrayt<float> ret(np::empty(arr1.rank(), arr1.dims(), arr1.itemtype()));
-  
-  const np::ssize_t *dims = arr1.dims();
-  for (int i=0; i<dims[0]; ++i)
-  {
-    ret(i) = arr1(i) + arr2(i);
-  }
-  return ret.getObject();
-}
-#endif
 
-#if BOOST_VERSION>106300
+
 py::object SumNumericArray(np::ndarray arr1, np::ndarray arr2)
 {
   //nm::array ret = py::extract<nm::array>(np::empty(1, &len, np::getItemtype(arr1)));
   //const Py_ssize_t len = py::extract<int>(py::getattr(arr1, "shape")[0]);
   //const Py_ssize_t len = arr1.get_nd();
-  py::tuple shape = py::make_tuple(1,10);
+  if( !(arr1.get_nd() == 1) )
+  {
+    throw std::invalid_argument("1d array expected");
+  }
+  py::tuple shape = py::make_tuple(arr1.shape(0));
   //np::dtype dtype = np::dtype::get_builtin<float>();
   np::ndarray ret = np::empty(shape,arr1.get_dtype());
   
-  for (int i=0; i<arr1.get_nd(); ++i)
+  for (int i=0; i<arr1.shape(0); ++i)
   {
     float x1 = py::extract<float>(arr1[i]);
     float x2 = py::extract<float>(arr2[i]);
@@ -388,46 +242,21 @@ py::object SumNumericArray(np::ndarray arr1, np::ndarray arr2)
   }
   return ret;
 }
-#else
-py::object SumNumericArray(nm::array arr1, nm::array arr2)
-{
-  const np::ssize_t len = py::extract<int>(py::getattr(arr1, "shape")[0]);
-  nm::array ret = py::extract<nm::array>(np::empty(1, &len, np::getItemtype(arr1)));
-  //py::tuple shape = py::make_tuple(1,len);
-  //np::dtype dtype = np::dtype::get_builtin<float>();
-  //np::ndarray ret = np::empty(shape,dtype);
-  for (int i=0; i<len; ++i)
-  {
-    float x1 = py::extract<float>(arr1[i]);
-    float x2 = py::extract<float>(arr2[i]);
-    ret[i] = py::object(x1 + x2);
-  }
-  return ret;
-}
-#endif
+
 
 
 BOOST_PYTHON_MODULE(libdemo)
 {
   Py_Initialize();
-#if BOOST_VERSION>106300
   cout<<"new boost numpy support detected"<<endl;
   np::initialize();
-  py::def("printConvertedArray_int", printConvertedArray);
-#else
-  cout<<"NO boost numpy support detected"<<endl;
-  np::importNumpyAndRegisterTypes();
   py::def("printConvertedArray_int", printConvertedArray<int>);
-  py::def("SumArrayT", SumArrayT);
-#endif
-  
+//   py::def("SumArrayT", SumArrayT);
   py::def("printArray", printArray);
-  
   py::def("int_to_str", scalar_to_str<int>);
   py::def("char_to_str", scalar_to_str<char>);
   py::def("float_to_str", scalar_to_str<float>);
   py::def("double_to_str", scalar_to_str<double>);
-  
   py::def("SumNumericArray", SumNumericArray);
   py::def("ReturnedFromCPP", ReturnedFromCPP);
 }
